a90dc8108fd40c74b4211dd488c88601761b367f
Ahmad Houri <Ahmad.hori@gmail.com>
2019-11-16 18:46:58 +0100

    Added Tests

--- HqPlusAssignment-Task1.Tests/HotelParserTests.cs
+++ HqPlusAssignment-Task1.Tests/HotelParserTests.cs
@@ -11,8 +11,9 @@ namespace HqPlusAssignment_Task1.Tests
         {
         }
 
+
         [TestCase("task 1 - Kempinski Hotel Bristol Berlin, Germany - Booking.com.html")]
-        public void ParserShouldBeValidWithAValidDocumentUrl(string documentUrl)
+        public void Should_ExtractData_When_Document_Is_Valid(string documentUrl)
         {
             HotelParser hotelParser = new HotelParser();
             hotelParser.fetchHtml(documentUrl);
@@ -23,6 +24,120 @@ namespace HqPlusAssignment_Task1.Tests
                 Assert.IsTrue(hotelParser.IsValid());
             }
         }
+
+        [TestCase("task 1 - Kempinski Hotel Bristol Berlin, Germany - Booking.com.html")]
+        public void Should_ExtractName_When_Document_Is_Valid(string documentUrl)
+        {
+            HotelParser hotelParser = new HotelParser();
+            hotelParser.fetchHtml(documentUrl);
+
+            if (hotelParser.RequestSucceeded)
+            {
+                hotelParser.ExtractName();
+                Assert.IsTrue(hotelParser.IsValid());
+            }
+        }
+
+        [TestCase("task 1 - Kempinski Hotel Bristol Berlin, Germany - Booking.com.html")]
+        public void Should_ExtractAddress_When_Document_Is_Valid(string documentUrl)
+        {
+            HotelParser hotelParser = new HotelParser();
+            hotelParser.fetchHtml(documentUrl);
+
+            if (hotelParser.RequestSucceeded)
+            {
+                hotelParser.ExtractAddress();
+                Assert.IsTrue(hotelParser.IsValid());
+            }
+        }
+
+        [TestCase("task 1 - Kempinski Hotel Bristol Berlin, Germany - Booking.com.html")]
+        public void Should_ExtractClassification_When_Document_Is_Valid(string documentUrl)
+        {
+            HotelParser hotelParser = new HotelParser();
+            hotelParser.fetchHtml(documentUrl);
+
+            if (hotelParser.RequestSucceeded)
+            {
+                hotelParser.ExtractClassification();
+                Assert.IsTrue(hotelParser.IsValid());
+            }
+        }
+
+        [TestCase("task 1 - Kempinski Hotel Bristol Berlin, Germany - Booking.com.html")]
+        public void Should_ExtractReviewPoints_When_Document_Is_Valid(string documentUrl)
+        {
+            HotelParser hotelParser = new HotelParser();
+            hotelParser.fetchHtml(documentUrl);
+
+            if (hotelParser.RequestSucceeded)
+            {
+                hotelParser.ExtractReviewPoints();
+                Assert.IsTrue(hotelParser.IsValid());
+            }
+        }
+
+        [TestCase("task 1 - Kempinski Hotel Bristol Berlin, Germany - Booking.com.html")]
+        public void Should_ExtractNumberOfReviews_When_Document_Is_Valid(string documentUrl)
+        {
+            HotelParser hotelParser = new HotelParser();
+            hotelParser.fetchHtml(documentUrl);
+
+            if (hotelParser.RequestSucceeded)
+            {
+                hotelParser.ExtractNumberOfReviews();
+                Assert.IsTrue(hotelParser.IsValid());
+            }
+        }
+
+        [TestCase("task 1 - Kempinski Hotel Bristol Berlin, Germany - Booking.com.html")]
+        public void Should_ExtractDescription_When_Document_Is_Valid(string documentUrl)
+        {
+            HotelParser hotelParser = new HotelParser();
+            hotelParser.fetchHtml(documentUrl);
+
+            if (hotelParser.RequestSucceeded)
+            {
+                hotelParser.ExtractDescription();
+                Assert.IsTrue(hotelParser.IsValid());
+            }
+        }
+
+        [TestCase("task 1 - Kempinski Hotel Bristol Berlin, Germany - Booking.com.html")]
+        public void Should_ExtractRoomCategories_When_Document_Is_Valid(string documentUrl)
+        {
+            HotelParser hotelParser = new HotelParser();
+            hotelParser.fetchHtml(documentUrl);
+
+            if (hotelParser.RequestSucceeded)
+            {
+                hotelParser.ExtractRoomCategories();
+                Assert.IsTrue(hotelParser.IsValid());
+            }
+        }
+
+        [TestCase("task 1 - Kempinski Hotel Bristol Berlin, Germany - Booking.com.html")]
+        public void Should_ExtractAlternativeHotels_When_Document_Is_Valid(string documentUrl)
+        {
+            HotelParser hotelParser = new HotelParser();
+            hotelParser.fetchHtml(documentUrl);
+
+            if (hotelParser.RequestSucceeded)
+            {
+                hotelParser.ExtractAlternativeHotels();
+                Assert.IsTrue(hotelParser.IsValid());
+            }
+        }
+
+        //Should_FailTo_ExtractData_When_Document_Is_Invalid
+        //Should_FailTo_ExtractName_When_Document_Is_Invalid
+        //Should_FailTo_ExtractAddress_When_Document_Is_Invalid
+        //Should_FailTo_ExtractClassification_When_Document_Is_Invalid
+        //Should_FailTo_ExtractReviewPoints_When_Document_Is_Invalid
+        //Should_FailTo_ExtractNumberOfReviews_When_Document_Is_Invalid
+        //Should_FailTo_ExtractDescription_When_Document_Is_Invalid
+        //Should_FailTo_ExtractRoomCategories_When_Document_Is_Invalid
+        //Should_FailTo_ExtractAlternativeHotels_When_Document_Is_Invalid
 
     }
 }
